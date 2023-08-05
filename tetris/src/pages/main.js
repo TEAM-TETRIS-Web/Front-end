f14061832a1f59aaa123aac2fea982978f926d49
sangkyu <doesol1301@gmail.com>
2023-08-05 23:49:14 +0900

    set main

--- tetris/src/pages/main.js
+++ tetris/src/pages/main.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from "react";
 
 const Main = () => {
   return (
-    <>
-    </>
+    <div className="container">
+      <h1>
+        안녕
+      </h1>
+    </div>
   )
 }
 
