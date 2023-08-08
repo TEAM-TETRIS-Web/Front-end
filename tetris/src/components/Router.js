9b87c70af281d1a3f1973defe4c712014424c815
sangkyu <doesol1301@gmail.com>
2023-08-09 02:40:06 +0900

    router single room 추가

--- tetris/src/components/Router.js
+++ tetris/src/components/Router.js
@@ -1,13 +1,14 @@
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Navigation from './Navigation';
 import Main from "../pages/main";
-
+import Single from "../pages/single";
 function AppRouter() {
   return (
     <>
       <Navigation />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/single" element={<Single/>} />
       </Routes>
     </>
   ) 
